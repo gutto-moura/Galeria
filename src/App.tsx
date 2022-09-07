0953e277e395a9ae844dd05f3cb73a487226e645
gutto-moura <gmourah1792@gmail.com>
2022-09-07 17:07:40 -0300

    Arquivos iniciais galeria

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as C from "./App.style";
+
+const App = () => {
+  // TEMPO : 30:01
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+          
+      </C.Area>
+    </C.Container>
+  )
+}
+export default App;
